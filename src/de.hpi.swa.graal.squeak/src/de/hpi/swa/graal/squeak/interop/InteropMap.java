497c4443c1259fc91c743a3990e8d103bea46ea4
Fabio Niephaus <code@fniephaus.com>
2018-12-03 12:27:09 +0100

    Add TruffleBoundaries [ci skip]

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/interop/InteropMap.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/interop/InteropMap.java
@@ -2,6 +2,7 @@ package de.hpi.swa.graal.squeak.interop;
 
 import java.util.Map;
 
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.interop.KeyInfo;
 import com.oracle.truffle.api.interop.MessageResolution;
@@ -33,6 +34,7 @@ public final class InteropMap implements TruffleObject {
 
     @Resolve(message = "KEY_INFO")
     abstract static class KeyInfoNode extends Node {
+        @TruffleBoundary
         public Object access(final InteropMap receiver, final Object key) {
             if (receiver.map.containsKey(key)) {
                 return KeyInfo.READABLE;
@@ -44,6 +46,7 @@ public final class InteropMap implements TruffleObject {
 
     @Resolve(message = "KEYS")
     abstract static class GetSize extends Node {
+        @TruffleBoundary
         public Object access(final InteropMap receiver) {
             return new InteropArray(receiver.map.keySet().toArray());
         }
@@ -51,6 +54,7 @@ public final class InteropMap implements TruffleObject {
 
     @Resolve(message = "READ")
     abstract static class Read extends Node {
+        @TruffleBoundary
         public Object access(final InteropMap receiver, final Object key) {
             return receiver.map.get(key);
         }
