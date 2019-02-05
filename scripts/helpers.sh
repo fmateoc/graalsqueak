1939af4ddca299b0f78db961d51070c497f0b705
Fabio Niephaus <code@fniephaus.com>
2019-02-05 16:20:28 +0100

    Test against new test image

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-readonly GIT_TAG="0.3.0"
-readonly IMAGE32_NAME="GraalSqueak-18163-32bit.zip"
-readonly IMAGE64_NAME="GraalSqueak-18163-64bit.zip"
+readonly GIT_TAG="0.5.0"
+readonly IMAGE32_NAME="Squeak5.3alpha-18436-32bit-TestImage.zip"
+readonly IMAGE64_NAME="Squeak5.3alpha-18436-64bit-TestImage.zip"
 readonly GITHUB_SLUG="hpi-swa-lab/graalsqueak"
 readonly MX_GIT="https://github.com/graalvm/mx.git"
 
