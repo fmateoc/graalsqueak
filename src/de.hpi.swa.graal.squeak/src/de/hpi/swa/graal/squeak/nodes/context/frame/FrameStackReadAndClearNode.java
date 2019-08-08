d7ca98f21f66414e77bcc9a1503d6ddf4eb445db
Fabio Niephaus <code@fniephaus.com>
2019-08-08 17:41:08 +0200

    Make FrameStackReadAndClearNode more efficient

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/frame/FrameStackReadAndClearNode.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/context/frame/FrameStackReadAndClearNode.java
@@ -1,25 +1,28 @@
 package de.hpi.swa.graal.squeak.nodes.context.frame;
 
 import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.frame.Frame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 
 import de.hpi.swa.graal.squeak.model.CompiledCodeObject;
 import de.hpi.swa.graal.squeak.model.ObjectLayouts.CONTEXT;
 import de.hpi.swa.graal.squeak.nodes.AbstractNodeWithCode;
+import de.hpi.swa.graal.squeak.util.ArrayUtils;
 import de.hpi.swa.graal.squeak.util.FrameAccess;
 
 @ReportPolymorphism
 @ImportStatic(CONTEXT.class)
 public abstract class FrameStackReadAndClearNode extends AbstractNodeWithCode {
+    @Children FrameSlotReadNode[] readNodes;
 
     protected FrameStackReadAndClearNode(final CompiledCodeObject code) {
         super(code);
+        readNodes = new FrameSlotReadNode[code.getNumStackSlots()];
     }
 
     public static FrameStackReadAndClearNode create(final CompiledCodeObject code) {
@@ -30,36 +33,54 @@ public abstract class FrameStackReadAndClearNode extends AbstractNodeWithCode {
         final int newSP = FrameAccess.getStackPointer(frame, code) - 1;
         assert newSP >= 0 : "Bad stack pointer";
         FrameAccess.setStackPointer(frame, code, newSP);
-        return execute(frame, newSP);
+        return execute(frame, newSP, 1)[0];
     }
 
     @ExplodeLoop
     public final Object[] executePopN(final VirtualFrame frame, final int numPop) {
         CompilerAsserts.compilationConstant(numPop);
-        final int currentSP = FrameAccess.getStackPointer(frame, code);
-        assert currentSP - numPop >= 0;
+        final int newSP = FrameAccess.getStackPointer(frame, code) - numPop;
+        assert newSP >= 0 : "Bad stack pointer";
+        FrameAccess.setStackPointer(frame, code, newSP);
+        return execute(frame, newSP, numPop);
+    }
+
+    protected abstract Object[] execute(VirtualFrame frame, int sp, int numPop);
+
+    @SuppressWarnings("unused")
+    @Specialization(guards = {"numPop == 0"})
+    protected static final Object[] doNone(final VirtualFrame frame, final int sp, final int numPop) {
+        return ArrayUtils.EMPTY_ARRAY;
+    }
+
+    @SuppressWarnings("unused")
+    @Specialization(guards = {"numPop == 1", "sp == cachedSP"}, limit = "MAX_STACK_SIZE")
+    protected final Object[] doSingle(final VirtualFrame frame, final int sp, final int numPop,
+                    @Cached("sp") final int cachedSP) {
+        return new Object[]{getReadNode(cachedSP).executeRead(frame)};
+    }
+
+    @SuppressWarnings("unused")
+    @ExplodeLoop
+    @Specialization(guards = {"numPop > 1", "sp == cachedSP"}, limit = "6")
+    protected final Object[] doMultiple(final VirtualFrame frame, final int sp, final int numPop,
+                    @Cached("sp") final int cachedSP) {
+        CompilerAsserts.compilationConstant(numPop);
         final Object[] result = new Object[numPop];
-        for (int i = 1; i <= numPop; i++) {
-            result[numPop - i] = execute(frame, currentSP - i);
-            assert result[numPop - i] != null;
+        for (int i = 0; i < numPop; i++) {
+            result[i] = getReadNode(cachedSP + i).executeRead(frame);
+            assert result[i] != null;
         }
-        FrameAccess.setStackPointer(frame, code, currentSP - numPop);
         return result;
     }
 
-    protected abstract Object execute(Frame frame, int stackIndex);
-
-    @SuppressWarnings("unused")
-    @Specialization(guards = {"index == cachedIndex"}, limit = "MAX_STACK_SIZE")
-    protected static final Object doClear(final Frame frame, final int index,
-                    @Cached("index") final int cachedIndex,
-                    @Cached("createReadNode(index)") final FrameSlotReadNode clearNode) {
-        return clearNode.executeRead(frame);
-    }
-
-    protected final FrameSlotReadNode createReadNode(final int index) {
-        // Only clear stack values, not receiver, arguments, or temporary variables.
-        final boolean clear = index >= code.getNumArgsAndCopied() + code.getNumTemps();
-        return FrameSlotReadNode.create(code.getStackSlot(index), clear);
+    protected final FrameSlotReadNode getReadNode(final int index) {
+        if (readNodes[index] == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            // Only clear stack values, not receiver, arguments, or temporary variables.
+            final boolean clear = index >= code.getNumArgsAndCopied() + code.getNumTemps();
+            readNodes[index] = insert(FrameSlotReadNode.create(code.getStackSlot(index), clear));
+        }
+        return readNodes[index];
     }
 }
