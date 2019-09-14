4808dd567421e8f9b59c096b82876b97bfac984b
Fabio Niephaus <code@fniephaus.com>
2019-09-14 19:16:53 +0200

    Cache codeObject in SenderForDispatchNode

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/SenderForDispatchNode.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/SenderForDispatchNode.java
@@ -1,13 +1,14 @@
 package de.hpi.swa.graal.squeak.nodes;
 
 import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 import de.hpi.swa.graal.squeak.model.CompiledCodeObject;
 
 public abstract class SenderForDispatchNode extends AbstractNodeWithCode {
+    protected static final int CACHE_LIMIT = 3;
+
     protected SenderForDispatchNode(final CompiledCodeObject code) {
         super(code);
     }
@@ -18,14 +19,21 @@ public abstract class SenderForDispatchNode extends AbstractNodeWithCode {
 
     public abstract Object execute(VirtualFrame frame, CompiledCodeObject codeObject);
 
+    @Specialization(guards = {"!codeObject.isUnwindMarked()", "codeObject == cachedCodeObject"}, assumptions = "cachedCodeObject.getDoesNotNeedSenderAssumption()", limit = "CACHE_LIMIT")
+    protected final Object doContextOrMarkerCached(final VirtualFrame frame, @SuppressWarnings("unused") final CompiledCodeObject codeObject,
+                    @SuppressWarnings("unused") @Cached("codeObject") final CompiledCodeObject cachedCodeObject) {
+        return getContextOrMarker(frame);
+    }
+
+    @Specialization(guards = {"!codeObject.isUnwindMarked()", "codeObject.getDoesNotNeedSenderAssumption().isValid()"}, replaces = "doContextOrMarkerCached")
+    protected final Object doContextOrMarker(final VirtualFrame frame, @SuppressWarnings("unused") final CompiledCodeObject codeObject,
+                    @SuppressWarnings("unused") @Cached("codeObject") final CompiledCodeObject cachedCodeObject) {
+        return getContextOrMarker(frame);
+    }
+
     @Specialization(guards = "codeObject.isUnwindMarked() || !codeObject.getDoesNotNeedSenderAssumption().isValid()")
     protected static final Object doEnsureContext(final VirtualFrame frame, @SuppressWarnings("unused") final CompiledCodeObject codeObject,
                     @Cached("create(code)") final GetOrCreateContextNode getOrCreateContextNode) {
         return getOrCreateContextNode.executeGet(frame);
-    }
-
-    @Fallback
-    protected final Object doContextOrMarker(final VirtualFrame frame, @SuppressWarnings("unused") final CompiledCodeObject codeObject) {
-        return getContextOrMarker(frame);
     }
 }
